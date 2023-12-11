1fbb063447e4a2889cb91e8b8019cb5c854cd43a
meiqi <meiqi.chen@cern.ch>
2023-12-11 15:39:40 +0000

    factorising adj mat generation

--- torch_adjacency_matrix.py
+++ torch_adjacency_matrix.py
@@ -15,12 +15,12 @@ logging.getLogger().setLevel(logging.INFO)
 import argparse
 import utils.normalisation as norm
 import utils.torch_distances as dis
+import utils.adj_mat as adj
 
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-#import torch_geometric
-#import torch_geometric.nn as geom_nn
+from torch.utils.data import DataLoader, TensorDataset
 
 import time
 st = time.time()
@@ -56,18 +56,6 @@ def GetParser():
       help="Specify sig-sig efficiency for the linking length",
   )
 
-  parser.add_argument(
-      "--ssbb",
-      action="store_true",
-      help="Specify linking length between sig-sig and bkg-bkg",
-  )
-
-  parser.add_argument(
-      "--sssb",
-      action="store_true",
-      help="Specify linking length between sig-sig and sig-bkg",
-  )
-
   args = parser.parse_args()
   return args
 
@@ -90,34 +78,8 @@ else:
 # load training data file and kinematics
 logging.info('Importing signal and background files...')
 file_path = "/data/atlas/atlasdata3/maggiechen/gnn_project/split_files/"
-df_sig = pd.read_hdf(file_path+"sig_train.h5", key="sig_train")
-df_sig = df_sig.sample(n=10)
-df_sig_wgts = df_sig["eventWeight"]
-sig_label = [1]*len(df_sig)
-df_sig = df_sig[kinematics]
-df_bkg = pd.read_hdf(file_path+"bkg_train.h5", key="bkg_train")
-df_bkg = df_bkg.sample(n=10)
-df_bkg_wgts = df_bkg["eventWeight"]
-bkg_label = [0]*len(df_bkg)
-df_bkg = df_bkg[kinematics]
-
-logging.info("MAD scaling...")
-# normalise kinematic values using MAD scaling
-for var in kinematics:
-    df_sig.loc[:, var], df_bkg.loc[:, var] = norm.MAD_norm(df_sig.loc[:, var], df_bkg.loc[:, var])
-
-logging.info("Converting torch tensors...")
-# convert pd dataframes to torch tensors
-torch_sig = torch.tensor(df_sig.values, dtype=torch.float32)
-torch_bkg = torch.tensor(df_bkg.values, dtype=torch.float32)
-torch_sig_wgts = torch.tensor(df_sig_wgts.values, dtype=torch.float32)
-torch_bkg_wgts = torch.tensor(df_bkg_wgts.values, dtype=torch.float32)
-# concatenating signal and background events / weights
-torch_all = torch.concat((torch_sig, torch_bkg), dim=0)
-truth_labels = torch.tensor(numpy.concatenate((sig_label, bkg_label)), dtype=torch.float32)
-torch_wgts = torch.concat((torch_sig_wgts, torch_bkg_wgts), dim=0)
-print("Shape of signal + background tensor", torch_all.size())
-
+train_x, train_wgts, train_truth_labels = adj.data_loader(file_path, "train", kinematics)
+val_x, val_wgts, val_truth_labels = adj.data_loader(file_path, "val", kinematics)
 
 # read in linking length calculated from sampled training data
 sigsig_eff = args.eff
@@ -125,60 +87,13 @@ with open('/data/atlas/atlasdata3/maggiechen/gnn_project/linking_lengths/'+args.
     length_dict = json.load(lfile)
     eff = length_dict["sigsig_eff"]
     ss_bb_lengths = length_dict["ss_bb_length"]
-    ss_sb_lengths = length_dict["ss_sb_length"]
-if args.ssbb:
     linking_length = ss_bb_lengths[eff.index(sigsig_eff)]
-elif args.sssb:
-    linking_length = ss_sb_lengths[eff.index(sigsig_eff)]
-else:
-    print("Please specify distributions to generate the linking length (ssbb or sssb)!")
 
-# calculate distances in chunks
-logging.info('Calculating distances in batches...')
+# calculate distances and generate adjacency matrix in batches
+logging.info('Calculating training and validaiton distances in batches...')
 chunksize = 20000
-nchunk = math.ceil(len(torch_all)/chunksize)
-
-def create_adj_mat(a, length):
-    return (a < length).float()
-
-
-def create_node_wgts(a, b):
-    a_col = a.view(-1,1)
-    b_col = b.view(1,-1)
-    outer = torch.matmul(a_col, b_col)
-    return torch.transpose(outer, 0, 1)
-
-# initialise adjacency matrix
-adj_mat = torch.empty((0, len(torch_all)))
-node_wgts = torch.empty((0, len(torch_wgts)))
-
-# calculating distances and cutting with linking length in chunks 
-for i in range(nchunk):
-    print("Batch number ", i)
-    # create subset of sig+bkg dataset
-    torch_all_subset = torch_all[(i*chunksize):(i+1)*chunksize]
-    torch_wgts_subset = torch_wgts[(i*chunksize):(i+1)*chunksize]
-    # calculate distances
-    if args.distance == "euclidean":
-        distance_subset = dis.euclidean(torch_all, torch_all_subset)
-    elif args.distance == "cityblock":
-        distance_subset = dis.cityblock(torch_all, torch_all_subset)
-    elif args.distance == "cosine":
-        distance_subset = dis.cosine(torch_all, torch_all_subset)
-
-    adj_mat_subset = create_adj_mat(distance_subset, linking_length)
-    print("adj mat subset", adj_mat_subset.size())
-    print("adj mat", adj_mat.size())
-    adj_mat = torch.concat((adj_mat_subset, adj_mat), dim=0)
-    
-    # get calculate node weights by batches
-    node_wgts_subset = create_node_wgts(torch_wgts, torch_wgts_subset)
-    print("node wgts subset", node_wgts_subset.size())
-    print("node wgts", node_wgts.size())
-    node_wgts = torch.concat((node_wgts_subset, node_wgts), dim=0)
-
-print(f"Time taken for adjacency matrix generation: {time.time() - st}")
-
+train_adj_mat = adj.generate_adj_mat(train_x, train_wgts, chunksize, args.distance, linking_length)
+val_adj_mat = adj.generate_adj_mat(val_x, val_wgts, chunksize, args.distance, linking_length)
 
 # Big ass GNN class like an adult
 class GCNLayer(nn.Module):
@@ -202,7 +117,6 @@ class GCNLayer(nn.Module):
     def forward(self, x, adjacency_matrix):
         weighted_node_features = torch.mul(x, self.node_weights[:, None])
         output = torch.matmul(adjacency_matrix, torch.matmul(weighted_node_features, self.train_weight)) + self.train_bias
-        #output = F.relu(output)
         return output
 
 class GCNClassifier(nn.Module):
@@ -226,8 +140,7 @@ class GCNClassifier(nn.Module):
     def forward(self, x, adjacency_matrix):
         for layer in self.hidden_layers:
             if isinstance(layer, GCNLayer):
-                x = layer(x, adjacency_matrix)
-                x = self.hidden_activation(x)
+               x = layer(x, adjacency_matrix)
             else:
                 x = layer(x)
                 x = self.hidden_activation(x)
@@ -238,19 +151,22 @@ class GCNClassifier(nn.Module):
 input_size = len(kinematics)
 hidden_sizes = [12, 12, 12]
 LR = 0.001
-epochs = 30
+epochs = 50
 
-gcn_model = GCNClassifier(input_size=input_size, hidden_sizes=hidden_sizes, output_size=1, node_weights=torch_wgts)
+
+# Load in training dataset, adjacency matrix, labels
+# Load in validation dataset, adjacency matrix, labels
+gcn_model = GCNClassifier(input_size=input_size, hidden_sizes=hidden_sizes, output_size=1, node_weights=train_wgts)
 train_loss = nn.CrossEntropyLoss()
 optimiser = torch.optim.Adam(gcn_model.parameters(), lr=LR)
 
 for epoch in range(epochs):
-    outputs = gcn_model(torch_all, adj_mat)
+    outputs = gcn_model(train_x, train_adj_mat)
     outputs = torch.sigmoid(outputs)
-    loss = train_loss(outputs.squeeze(), truth_labels.squeeze())
+    loss = train_loss(outputs.squeeze(), train_truth_labels.squeeze())
     optimiser.zero_grad()
     loss.backward()
     optimiser.step()
     print(f'Epoch {epoch + 1}/{epochs}, Loss: {loss.item()}')
 print("Final predictions", outputs)
-print("Truth labels", truth_labels)
+print("Truth labels", train_truth_labels)
---
+++ utils/adj_mat.py
@@ -0,0 +1,73 @@
+import numpy
+import pandas as pd
+import math
+import torch
+import time
+st = time.time()
+import utils.normalisation as norm
+import utils.torch_distances as dis
+
+def data_loader(file_path, f_type, kinematics):
+    df_sig =  pd.read_hdf(file_path+"sig_"+str(f_type)+".h5", key="sig_"+str(f_type))
+    df_bkg =  pd.read_hdf(file_path+"bkg_"+str(f_type)+".h5", key="bkg_"+str(f_type))
+    df_sig = df_sig.sample(n=10)
+    df_bkg = df_bkg.sample(n=10)
+    df_sig_wgts = df_sig["eventWeight"]
+    df_bkg_wgts = df_bkg["eventWeight"]
+    df_sig = df_sig[kinematics]
+    df_bkg = df_bkg[kinematics]
+    sig_label = [1]*len(df_sig)
+    bkg_label = [0]*len(df_bkg)
+
+    # MAD scaling
+    for var in kinematics:
+        df_sig.loc[:, var], df_bkg.loc[:, var] = norm.MAD_norm(df_sig.loc[:, var], df_bkg.loc[:, var])
+
+    # convert pd dataframes to torch tensors
+    torch_sig = torch.tensor(df_sig.values, dtype=torch.float32)
+    torch_bkg = torch.tensor(df_bkg.values, dtype=torch.float32)
+    torch_sig_wgts = torch.tensor(df_sig_wgts.values, dtype=torch.float32)
+    torch_bkg_wgts = torch.tensor(df_bkg_wgts.values, dtype=torch.float32)
+    # concatenating signal and background events / weights
+    torch_all = torch.concat((torch_sig, torch_bkg), dim=0)
+    torch_wgts = torch.concat((torch_sig_wgts, torch_bkg_wgts), dim=0)
+    truth_labels = torch.tensor(numpy.concatenate((sig_label, bkg_label)), dtype=torch.float32)
+
+    return torch_all, torch_wgts, truth_labels
+
+def create_adj_mat(a, length):
+    return (a < length).float()
+
+def create_node_wgts(a, b):
+    a_col = a.view(-1,1)
+    b_col = b.view(1,-1)
+    outer = torch.matmul(a_col, b_col)
+    return torch.transpose(outer, 0, 1)
+
+def generate_adj_mat(x, x_wgts, chunksize, dis_type, linking_length):
+    nchunk = math.ceil(len(x)/chunksize)
+    # initialise adjacency matrix
+    adj_mat = torch.empty((0, len(x)))
+    node_wgts = torch.empty((0, len(x_wgts)))
+
+    for i in range(nchunk):
+        # create subset of sig+bkg dataset
+        x_subset = x[(i*chunksize):(i+1)*chunksize]
+        x_wgts_subset = x_wgts[(i*chunksize):(i+1)*chunksize]
+        # calculate distances
+        if dis_type == "euclidean":
+            distance_subset = dis.euclidean(x, x_subset)
+        elif dis_type == "cityblock":
+            distance_subset = dis.cityblock(x, x_subset)
+        elif dis_type == "cosine":
+            distance_subset = dis.cosine(x, x_subset)
+
+        adj_mat_subset = create_adj_mat(distance_subset, linking_length)
+        adj_mat = torch.concat((adj_mat_subset, adj_mat), dim=0)
+        
+        # get calculate node weights by batches
+        #node_wgts_subset = adj.create_node_wgts(x_wgts, x_wgts_subset)
+        #node_wgts = torch.concat((node_wgts_subset, node_wgts), dim=0)
+
+    print(f"Time taken for adjacency matrix generation: {time.time() - st}")
+    return adj_mat
