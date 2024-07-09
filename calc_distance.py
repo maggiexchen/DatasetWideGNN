38cb87e418b3c79a79ac014d0d84a02e52e0aba1
meiqi <meiqi.chen@cern.ch>
2024-07-09 13:57:10 +0100

    adding test dataset to full x

--- calc_distance.py
+++ calc_distance.py
@@ -81,19 +81,20 @@ logging.info('Importing signal and background files...')
 SF_4b5b = 0.07 # placeholder value for HHH data-driven background, MC backgrounds would take eventWeights instead
 train_sig, train_bkg, train_x, train_sig_wgts, train_bkg_wgts, train_truth_sig_labels, train_truth_bkg_labels = adj.data_loader(h5_path, "", "train", kinematics, signal=signal)
 val_sig, val_bkg, val_x, val_sig_wgts, val_bkg_wgts, val_truth_sig_labels, val_truth_bkg_labels = adj.data_loader(h5_path, "", "val", kinematics, signal=signal)
-full_sig = torch.cat((train_sig, val_sig), dim=0)
-full_bkg = torch.cat((train_bkg, val_bkg), dim=0)
-sig_wgt = torch.cat((train_sig_wgts, val_sig_wgts), dim=0)
-train_truth_labels = torch.cat((train_truth_sig_labels, train_truth_bkg_labels), dim=0)
-val_truth_labels = torch.cat((val_truth_sig_labels, val_truth_bkg_labels), dim=0)
+test_sig, test_bkg, test_x, test_sig_wgts, test_bkg_wgts, test_truth_sig_labels, test_truth_bkg_labels = adj.data_loader(h5_path, "", "test", kinematics, signal=signal)
+
+full_sig = torch.cat((train_sig, val_sig, test_sig), dim=0)
+full_bkg = torch.cat((train_bkg, val_bkg, test_bkg), dim=0)
+sig_wgt = torch.cat((train_sig_wgts, val_sig_wgts, test_sig_wgts), dim=0)
+# train_truth_labels = torch.cat((train_truth_sig_labels, train_truth_bkg_labels), dim=0)
+# val_truth_labels = torch.cat((val_truth_sig_labels, val_truth_bkg_labels), dim=0)
+# test_truth_labels = torch.cat((test_truth_sig_labels, test_truth_bkg_labels), dim=0)
 global_bkg_wgt = 1.0
 if signal == "hhh": global_bkg_wgt = SF_4b5b
-bkg_wgt = torch.cat((train_bkg_wgts, val_bkg_wgts), dim=0)*global_bkg_wgt
+bkg_wgt = torch.cat((train_bkg_wgts, val_bkg_wgts, test_bkg_wgts), dim=0)*global_bkg_wgt
 
 # calculate distances in batches
 logging.info('Calculating distances in batches...')
-print("The number of signal events ", len(full_sig))
-print("The number of background events", len(full_bkg))
 
 def distance_calc(a, b, metric):
     if metric == "euclidean":
