c08d494ae7498f1cef53a221cc095a2f4c8c577e
meiqi <meiqi.chen@cern.ch>
2024-08-12 11:40:22 +0100

    clean up model and correct loss definitions

--- GraphBuilder/define_pair.py
+++ GraphBuilder/define_pair.py
@@ -24,13 +24,17 @@ class PairDataset(Dataset):
 
         sampled_sig_ind = torch.arange(self.num_sig_samples)
         sampled_bkg_ind = torch.arange(self.num_bkg_samples)
-        cartesian_product_sigbkg_ind = torch.cartesian_prod(sampled_sig_ind, sampled_bkg_ind)
         cartesian_product_sigsig_ind = torch.cartesian_prod(sampled_sig_ind, sampled_sig_ind)
+        cartesian_product_bkgbkg_ind = torch.cartesian_prod(sampled_bkg_ind, sampled_bkg_ind)
+        cartesian_product_sigbkg_ind = torch.cartesian_prod(sampled_sig_ind, sampled_bkg_ind)
 
         pairs = []
 
         for idx1, idx2 in cartesian_product_sigsig_ind:
             pairs.append((self.signal_events[sig_idx[idx1]], self.signal_events[sig_idx[idx2]], 1))
+        
+        for idx1, idx2 in cartesian_product_bkgbkg_ind:
+            pairs.append((self.background_events[bkg_idx[idx1]], self.background_events[bkg_idx[idx2]], 1))
         
         for idx1, idx2 in cartesian_product_sigbkg_ind:
             pairs.append((self.signal_events[sig_idx[idx1]], self.background_events[bkg_idx[idx2]], 0))
@@ -43,38 +47,3 @@ class PairDataset(Dataset):
     def __getitem__(self, idx):
         signal_event, background_event, label = self.pairs[idx]
         return signal_event, background_event, label
-
-"""
-
-class PairDataset(Dataset):
-    def __init__(self, signal_events, background_events, num_samples, num_negatives_per_positive):
-        self.signal_events = signal_events
-        self.background_events = background_events
-        self.num_samples = num_samples
-        self.num_negatives_per_positive = num_negatives_per_positive
-
-    def __len__(self):
-        return self.num_samples
-
-    def __getitem__(self, idx):
-        # Choose a random signal event
-        signal_idx = random.randint(0, len(self.signal_events) - 1)
-        signal_event = self.signal_events[signal_idx]
-
-        # Positive pair: another signal event
-        positive_idx = random.randint(0, len(self.signal_events) - 1)
-        positive_event = self.signal_events[positive_idx]
-        positive_label = 1
-
-        # Negative pairs: background events
-        negative_events = []
-        for _ in range(self.num_negatives_per_positive):
-            negative_idx = random.randint(0, len(self.background_events) - 1)
-            negative_event = self.background_events[negative_idx]
-            negative_events.append((signal_event, negative_event, 0))
-
-        # Create the sample
-        sample = [(signal_event, positive_event, positive_label)] + negative_events
-        return sample
-
-"""
--- GraphBuilder/embedding.py
+++ GraphBuilder/embedding.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.optim as optim
+import torch.nn.functional as F
 from torch.utils.data import DataLoader, Dataset
 
 class EventDataset(Dataset):
@@ -17,20 +18,24 @@ class EventDataset(Dataset):
 class EmbeddingNet(nn.Module):
     def __init__(self, input_dim, embedding_dim):
         super(EmbeddingNet, self).__init__()
-        self.fc1 = nn.Linear(input_dim, 128)
-        self.fc2 = nn.Linear(128, embedding_dim)
+        self.embedding_dim = embedding_dim
+        self.fc1 = nn.Linear(input_dim, 64)
+        self.fc2 = nn.Linear(64, 64)
+        self.fc3 = nn.Linear(64, embedding_dim)
 
     def forward(self, x):
         x = torch.relu(self.fc1(x))
-        x = self.fc2(x)
+        x = torch.relu(self.fc2(x))
+        x = self.fc3(x)
         return x
 
 class ContrastiveHingeLoss(nn.Module):
-    def __init__(self, margin=1.0):
+    def __init__(self, embedding_dim, margin=1.0):
         super(ContrastiveHingeLoss, self).__init__()
         self.margin = margin
-
+        self.embedding_dim = embedding_dim
+    
     def forward(self, output1, output2, label):
-        distances = torch.norm(output1 - output2, p=2, dim=1)
-        losses = 0.5 * (label * distances ** 2 + (1 - label) * torch.relu(self.margin - distances) ** 2)
-        return losses.mean()
+        distance = F.pairwise_distance(output1, output2, p=2)
+        loss = label * torch.pow(distance, 2) + (1-label) * torch.pow(F.relu(abs(self.margin-distance)),2)
+        return loss.sum()
