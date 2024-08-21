84605fab8f6ac1a045d82a4372752631ffdc9b4e
meiqi <meiqi.chen@cern.ch>
2024-08-21 14:04:29 +0100

    bash script to scan through parameters

---
+++ GraphBuilder/train.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Define the path to the YAML file
+CONFIG_FILE="config/user_Maggie.yaml"
+
+# Define the ranges or lists of values you want to scan through
+MARGINS=(0.1 0.5 1.0 2.0)
+EMBEDDING_DIMS=(4)
+
+# Loop through the parameter values
+for margin in "${MARGINS[@]}"; do
+    for embedding_dim in "${EMBEDDING_DIMS[@]}"; do
+        echo "Running with margin=$margin, embedding_dim=$embedding_dim"
+
+        # Update the YAML file directly with the new parameter values
+        sed -i "s/^margin: .*/margin: $margin/" "$CONFIG_FILE"
+        sed -i "s/^embedding_dim: .*/embedding_dim: $embedding_dim/" "$CONFIG_FILE"
+        python train.py -v LQ -u "$CONFIG_FILE"
+    done
+done
