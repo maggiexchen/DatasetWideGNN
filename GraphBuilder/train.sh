f6786c511a5d664e3e7e74561fea3a4934dfea19
meiqi <meiqi.chen@cern.ch>
2024-09-10 12:16:07 +0100

    bash script for parameter scan

--- GraphBuilder/train.sh
+++ GraphBuilder/train.sh
@@ -4,17 +4,20 @@
 CONFIG_FILE="config/user_Maggie.yaml"
 
 # Define the ranges or lists of values you want to scan through
-MARGINS=(0.1 0.5 1.0 2.0)
-EMBEDDING_DIMS=(4)
-
+MARGINS=(0.1 0.5 1.0 1.5 2)
+EMBEDDING_DIMS=(2 3 4 10 24)
+PENALTY=(1 2 3 4)
 # Loop through the parameter values
 for margin in "${MARGINS[@]}"; do
     for embedding_dim in "${EMBEDDING_DIMS[@]}"; do
-        echo "Running with margin=$margin, embedding_dim=$embedding_dim"
+        for penalty in "${PENALTY[@]}"; do
+            echo "Running with margin=$margin, embedding_dim=$embedding_dim, penalty=$penalty"
 
-        # Update the YAML file directly with the new parameter values
-        sed -i "s/^margin: .*/margin: $margin/" "$CONFIG_FILE"
-        sed -i "s/^embedding_dim: .*/embedding_dim: $embedding_dim/" "$CONFIG_FILE"
-        python train.py -v LQ -u "$CONFIG_FILE"
+            # Update the YAML file directly with the new parameter values
+            sed -i "s/^margin: .*/margin: $margin/" "$CONFIG_FILE"
+            sed -i "s/^embedding_dim: .*/embedding_dim: $embedding_dim/" "$CONFIG_FILE"
+            sed -i "s/^penalty: .*/penalty: $penalty/" "$CONFIG_FILE"
+            python train.py -v LQ -u "$CONFIG_FILE"
+        done
     done
 done
