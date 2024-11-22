1fff596bf11a1c37670dab587e6e647ba226afce
Maggie Chen <meiqi.chen@cern.ch>
2024-11-22 14:51:15 +0000

    removing train val test split

--- write_files.py
+++ write_files.py
@@ -37,58 +37,32 @@ user_config_path = args.userconfig
 user_config = misc.load_config(user_config_path)
 h5_path = user_config["h5_path"]
 ntuple_path = user_config["ntuple_path"]
+os.makedirs(h5_path, exist_ok=True)
 
 # TODO: assert. This should be "hhh" "LQ" or "stau"
 signal = user_config["signal"]
+backgrounds = user_config["backgrounds"]
 
 logging.info("signal: "+signal)
+logging.info("backgrounds: "+str(backgrounds))
 logging.info("input ntuple path: "+ntuple_path)
 logging.info("output h5 data path: "+h5_path)
 
 # load in input files
-logging.info('Importing signal and background files...')
-#signal_file = uproot.open("/data/atlas/atlasdata3/maggiechen/HHH6b_pairing/post-processing/20231008/6b_resonant_TRSM/out3_reco_6j_521176.root:tree")
-#background_file = uproot.open("/data/atlas/atlasdata3/maggiechen/HHH6b_pairing/post-processing/20231008/data/out3_data_reco_5j.root:tree")
-signal_file = uproot.open(ntuple_path + "6b_resonant_TRSM/out3_reco_6j_521176.root:tree")
-background_file = uproot.open(ntuple_path + "data/out3_data_reco_5j.root:tree")
+logging.info('Importing and writing signal '+str(signal)+' ...')
+signal_file = uproot.open(ntuple_path + "GNNTree_"+str(signal)+".root:tree")
 features = signal_file.keys()
-df_sig = signal_file.arrays(library="pd")
-df_bkg = background_file.arrays(library="pd")
+df_sig = {str(signal):{}}
+df_sig[signal] = signal_file.arrays(library="pd")
+df_sig[signal]["target"] = [1]*len(df_sig[signal])
+df_sig[signal].to_hdf(h5_path + str(signal)+".h5", key=str(signal), mode="w")
 
-# split into train validation and test here
-logging.info('Train/validation/test splitting...')
-y_sig = [0]*len(df_sig["eventWeight"])
-y_bkg = [1]*len(df_bkg["eventWeight"])
-
-# train:validation:test = 5:3:2
-x_sig_train_val, x_sig_test, y_sig_train_val, y_sig_test = train_test_split(df_sig, y_sig, test_size=0.2, shuffle=False)
-x_sig_train, x_sig_val, y_sig_train, y_sig_val = train_test_split(x_sig_train_val, y_sig_train_val, test_size=0.375, shuffle=False)
-
-x_bkg_train_val, x_bkg_test, y_bkg_train_val, y_bkg_test = train_test_split(df_bkg, y_bkg, test_size=0.2, shuffle=False)
-x_bkg_train, x_bkg_val, y_bkg_train, y_bkg_val = train_test_split(x_bkg_train_val, y_bkg_train_val, test_size=0.375, shuffle=False)
-
-logging.info("Creating dataFrames")
-
-df_sig_train = pd.DataFrame({str(var): x_sig_train[var] for var in features})
-df_sig_val = pd.DataFrame({str(var): x_sig_val[var] for var in features})
-df_sig_test = pd.DataFrame({str(var): x_sig_test[var] for var in features})
-df_bkg_train = pd.DataFrame({str(var): x_bkg_train[var] for var in features})
-df_bkg_val = pd.DataFrame({str(var): x_bkg_val[var] for var in features})
-df_bkg_test = pd.DataFrame({str(var): x_bkg_test[var] for var in features})
-
-df_sig_train["target"] = y_sig_train
-df_sig_val["target"] = y_sig_val
-df_sig_test["target"] = y_sig_test
-df_bkg_train["target"] = y_bkg_train
-df_bkg_val["target"] = y_bkg_val
-df_bkg_test["target"] = y_bkg_test
-
-#save_path = "/data/atlas/atlasdata3/maggiechen/gnn_project/split_files"
-logging.info("Saving h5 files")
-df_sig_train.to_hdf(h5_path + "sig_train.h5", key="sig_train", mode="w")
-df_sig_val.to_hdf(h5_path + "sig_val.h5", key="sig_val", mode="w")
-df_sig_test.to_hdf(h5_path + "sig_test.h5", key="sig_test", mode="w")
-df_bkg_train.to_hdf(h5_path + "bkg_train.h5", key="bkg_train", mode="w")
-df_bkg_val.to_hdf(h5_path + "bkg_val.h5", key="bkg_val", mode="w")
-df_bkg_test.to_hdf(h5_path + "bkg_test.h5", key="bkg_test", mode="w")
-
+logging.info('Importing and writing background ')
+df_bkgs = {}
+for background in backgrounds:
+    logging.info(str(background)+" ...")
+    df_bkgs[str(background)] = {}
+    background_file = uproot.open(ntuple_path + "GNNTree_"+str(background)+".root:tree")
+    df_bkgs[background] = background_file.arrays(library="pd")
+    df_bkgs[background]["target"] = [0]*len(df_bkgs[background])
+    df_bkgs[background].to_hdf(h5_path + str(background)+".h5", key=str(background), mode="w")
