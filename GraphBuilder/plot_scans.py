6685230845014dd20cb2a906bafd1ad03ef059a5
Maggie Chen <meiqi.chen@cern.ch>
2024-12-06 11:23:14 +0000

    major plotting update

--- GraphBuilder/plot_scans.py
+++ GraphBuilder/plot_scans.py
@@ -3,9 +3,12 @@ import sys
 parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 sys.path.append(parent_dir)
 import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
 import json
 import utils.misc as misc
 import argparse
+from glob import glob
 
 def GetParser():
     """Argument parser for reading Ntuples script."""
@@ -22,11 +25,25 @@ def GetParser():
     )
 
     parser.add_argument(
-        "--plot",
-        "-p",
+        "--scan",
+        "-s",
+        action="store_true",
+        help="Plot scan over model parameters, need to then specify which parameter to fix in these scans",
+    )
+    
+    parser.add_argument(
+        "--fixed",
+        "-f",
         type=str,
-        required=True,
-        help="Specify which type of plot to make (margin, penalty)",
+        required=False,
+        help="Specify which parameter to fix (margin, penalty, dim)",
+    )
+
+    parser.add_argument(
+        "--corr",
+        "-c",
+        action="store_true",
+        help="Plot correlation between parameters and metrics"
     )
 
     args = parser.parse_args()
@@ -37,148 +54,118 @@ user_config_path = args.userconfig
 user_config = misc.load_config(user_config_path)
 plot_path = user_config["plot_path"]
 model_path = user_config["model_path"]
-margins = [0.1, 0.5, 1.0, 2.0]
+margins = [0.1, 0.5, 1.0, 2.0, 2.5, 3.0]
 penalties = [1, 5, 10, 20]
-dims = [2, 4, 12]
-dims_colours = ["dodgerblue", "firebrick", "darkorchid", "forestgreen", "darkorange"]
-pen_colours = ["thistle", "plum", "mediumorchid", "darkviolet", "blueviolet", "rebeccapurple", "indigo"]
+dims = [2, 4, 12, 16, 18, 20]
+parameter_dict = {"margin": {"value": margins, "label": "Margin"}, 
+                  "penalty": {"value": penalties, "label": "Penalty"},
+                   "dim": {"value": dims, "label": "Dimension"}}
+colours = ["dodgerblue", "firebrick", "darkorchid", "forestgreen", "darkorange", "plum"]
+gradient_colours = ["thistle", "plum", "mediumorchid", "darkviolet", "blueviolet", "rebeccapurple", "indigo"]
 linestyles = ["-", "--", "dotted"]
-
-if args.plot == "margin":
-    eff_fig, eff_ax = plt.subplots(figsize=(11, 9))
-    pur_fig, pur_ax = plt.subplots(figsize=(11, 9))
-    for i, dim in enumerate(dims):
-        same_class_eff = []
-        same_class_pur = []
-        sigsig_eff = []
-        sigsig_pur = []
-        bkgbkg_eff = []
-        bkgbkg_pur = []
-
-        for margin in margins: 
-            penalty = 1
-            radius = margin/2
-            json_file = model_path + "embedding_"+str(dim)+"feats/EmbeddingNet_m"+str(margin)+"_r"+str(radius)+"_Lambda"+str(penalty)+"_dict.json"
-            with open(json_file, 'r') as file:
-                graph_dict = json.load(file)
-
-            same_class_eff.append(graph_dict["same_class_eff"])
-            same_class_pur.append(graph_dict["same_class_purity"])
-            sigsig_eff.append(graph_dict["sigsig_eff"])
-            sigsig_pur.append(graph_dict["sigsig_purity"])
-            bkgbkg_eff.append(graph_dict["bkgbkg_eff"])
-            bkgbkg_pur.append(graph_dict["bkgbkg_purity"])
-        
-        eff_ax.plot(margins, same_class_eff, linestyle=linestyles[0], marker='o', color=dims_colours[i])
-        eff_ax.plot(margins, sigsig_eff, linestyle=linestyles[1], marker='o', color=dims_colours[i])
-        eff_ax.plot(margins, bkgbkg_eff, linestyle=linestyles[2], marker='o', color=dims_colours[i])
-        
-        linestyle_leg = [plt.Line2D([0], [0], color="black", linestyle=l) for l in linestyles]
-        colour_leg = [plt.Line2D([0], [0], lw=2, linestyle="-", marker='o', color=c) for c in dims_colours]
-        
-        eff_ax.set_xlabel("Margin", loc="right", fontsize=16)
-        eff_ax.set_ylabel("Efficiency", loc="top", fontsize=16)
-        linestyle_legend = eff_ax.legend(handles=linestyle_leg, labels=["Same class", "Sig-sig", "Bkg-bkg"], fontsize=16, frameon=False, loc="upper center")
-        colour_legend = eff_ax.legend(handles=colour_leg, labels=[str(d)+" embeddings" for d in dims], fontsize=16, frameon=False, loc="upper right")
-        eff_ax.add_artist(linestyle_legend)
-        eff_ax.set_yscale("log")
-        eff_ax.set_ylim((0.7, 1.05))
-        os.makedirs(plot_path, exist_ok=True)
-        eff_fig.savefig(plot_path+"eff_vs_margin.pdf")
-
-        pur_ax.plot(margins, same_class_pur, linestyle=linestyles[0], marker='^', color=dims_colours[i])
-        pur_ax.plot(margins, sigsig_pur, linestyle=linestyles[1], marker='^', color=dims_colours[i])
-        pur_ax.plot(margins, bkgbkg_pur, linestyle=linestyles[2], marker='^', color=dims_colours[i])
-
-        pur_ax.set_xlabel("Margin", loc="right", fontsize=16)
-        pur_ax.set_ylabel("Purity", loc="top", fontsize=16)
-        linestyle_legend = pur_ax.legend(handles=linestyle_leg, labels=["Same class", "Sig-sig", "Bkg-bkg"], fontsize=16, frameon=False, loc="upper center")
-        colour_legend = pur_ax.legend(handles=colour_leg, labels=[str(d)+" embeddings" for d in dims], fontsize=16, frameon=False, loc="upper right")
-        pur_ax.add_artist(linestyle_legend)
-        pur_ax.set_yscale("log")
-        pur_ax.set_ylim((0.3, 1.4))
-        os.makedirs(plot_path, exist_ok=True)
-        pur_fig.savefig(plot_path+"purity_vs_margin.pdf")
-
-if args.plot == "penalty":
-    eff_fig, eff_ax = plt.subplots(figsize=(11, 9))
-    pur_fig, pur_ax = plt.subplots(figsize=(11, 9))
-    dist_fig, dist_ax = plt.subplots(figsize=(11, 9))
-    avg_sigsig_dist = []
-    avg_bkgbkg_dist = []
-    avg_sigbkg_dist = []
-    sigsig_eff = []
-    bkgbkg_eff = []
-    # sigbkg_eff = []
-    sigsig_pur = []
-    bkgbkg_pur = []
-    # sigbkg_pur = []
-    for i, pen in enumerate(penalties):
-        dim = 2
-        margin = 0.1
-        radius = margin/2
-        json_file = model_path + "embedding_"+str(dim)+"feats/EmbeddingNet_m"+str(margin)+"_r"+str(radius)+"_Lambda"+str(pen)+"_dict.json"
-        with open(json_file, 'r') as file:
-            graph_dict = json.load(file)
-        sigsig_eff.append(graph_dict["sigsig_eff"])
-        sigsig_pur.append(graph_dict["sigsig_purity"])
-        bkgbkg_eff.append(graph_dict["bkgbkg_eff"])
-        bkgbkg_pur.append(graph_dict["bkgbkg_purity"])
-        # sigbkg_eff.append(graph_dict["sigbkg_eff"])
-        # sigbkg_pur.append(graph_dict["sigbkg_purity"])
-        avg_sigsig_dist.append(graph_dict["avg_sigsig_dist"])
-        avg_bkgbkg_dist.append(graph_dict["avg_bkgbkg_dist"])
-        avg_sigbkg_dist.append(graph_dict["avg_sigbkg_dist"])
-    eff_ax.plot(penalties, sigsig_eff,marker='o', label="Sig-sig")
-    eff_ax.plot(penalties, bkgbkg_eff,marker='o', label="Bkg-bkg")
-    # eff_ax.plot(penalties, sigbkg_eff, linestyle=linestyles[2], marker='o', color=pen_colours[i])
-
-    pur_ax.plot(penalties, sigsig_pur, marker='^', label="Sig-sig")
-    pur_ax.plot(penalties, bkgbkg_pur, marker='^', label="Bkg-bkg")
-    # pur_ax.plot(penalties, sigbkg_pur, linestyle=linestyles[2], marker='^', color=pen_colours[i])
-
-    dist_ax.plot(penalties, avg_sigsig_dist, marker='P', label="Sig-sig")
-    dist_ax.plot(penalties, avg_bkgbkg_dist, marker='P', label="Bkg-bkg")
-    dist_ax.plot(penalties, avg_sigbkg_dist, marker='P', label="Sig-bkg")
+s_type = ["Sig-sig", "Bkg-bkg", "Sig-bkg"]
+parameters = ["margin", "penalty", "dim"]
 
 
-    # linestyle_leg = [plt.Line2D([0], [0], color="black", linestyle=l) for l in linestyles]
-    # colour_leg = [plt.Line2D([0], [0], lw=2, linestyle="-", marker='o', color=c) for c in pen_colours]
-    # eff_ax.set_xlabel(r"Penalty $\lambda$", loc="right", fontsize=16)
-    # eff_ax.set_ylabel("Efficiency", loc="top", fontsize=16)
-    # linestyle_legend = eff_ax.legend(handles=linestyle_leg, labels=["Sig-sig", "Bkg-bkg"], fontsize=16, frameon=False, loc="upper center")
-    # colour_legend = eff_ax.legend(handles=colour_leg, labels="2 embeddings", fontsize=16, frameon=False, loc="upper right")
-    # eff_ax.add_artist(linestyle_legend)
-    eff_ax.set_yscale("log")
-    eff_ax.legend(loc="upper right", fontsize=16)
+def assign_variable(parameters, values, assignment):
+    for i, parameter in enumerate(parameters):
+        if parameter == assignment:
+            variable = values[i]
+    return variable
 
-    pur_ax.set_xlabel(r"Penalty $\lambda$", loc="right", fontsize=16)
-    pur_ax.set_ylabel("Purity", loc="top", fontsize=16)
-    pur_ax.legend(loc="upper right", fontsize=16)
-    # linestyle_legend = pur_ax.legend(handles=linestyle_leg, labels=["Sig-sig", "Bkg-bkg"], fontsize=16, frameon=False, loc="upper center")
-    # colour_legend = pur_ax.legend(handles=colour_leg, labels="2 embeddings", fontsize=16, frameon=False, loc="upper right")
-    # pur_ax.add_artist(linestyle_legend)
-    pur_ax.set_yscale("log")
 
-    dist_ax.set_xlabel(r"Penalty $\lambda$", loc="right", fontsize=16)
-    dist_ax.set_ylabel("Average distance", loc="top", fontsize=16)
-    dist_ax.legend(loc="upper right", fontsize=16)
-    # linestyle_legend = dist_ax.legend(handles=linestyle_leg, labels=["Sig-sig", "Bkg-bkg", "Sig-bkg"], fontsize=16, frameon=False, loc="upper center")
-    # colour_legend = dist_ax.legend(handles=colour_leg, labels="2 embeddings", fontsize=16, frameon=False, loc="upper right")
-    # dist_ax.add_artist(linestyle_legend)
 
-    os.makedirs(plot_path, exist_ok=True)
-    eff_fig.savefig(plot_path+"eff_vs_penalty.pdf")
-    pur_fig.savefig(plot_path+"purity_vs_penalty.pdf")
-    dist_fig.savefig(plot_path+"distance_vs_penalty.pdf")
+if args.scan:
+    if args.fixed == False:
+        print("Need to specify a pamaeter to fix")
+    else:
+        if args.fixed not in parameters:
+            print("Need to specify a valid parameter to fix")
+        else:
+            parameters_to_plot = [s for s in parameters if s != args.fixed]
+
+        print("Fixing ", args.fixed, ", scanning over", parameters_to_plot)
+
+    for i, i_value in enumerate(parameter_dict[args.fixed]["value"]):
+        label_i = parameter_dict[args.fixed]["label"]
+        for parameter_j in parameters_to_plot:
+            eff_fig, eff_ax = plt.subplots(figsize=(11, 9))
+            pur_fig, pur_ax = plt.subplots(figsize=(11, 9))
+            dist_fig, dist_ax = plt.subplots(figsize=(11, 9))
+            label_j = parameter_dict[parameter_j]["label"]
+            parameter_k = parameters_to_plot[1] if parameter_j == parameters_to_plot[0] else parameters_to_plot[0]
+            label_k = parameter_dict[parameter_k]["label"]
+            for j, j_value in enumerate(parameter_dict[parameter_j]["value"]):
+                same_class_eff = []
+                same_class_pur = []
+                sigsig_eff = []
+                sigsig_pur = []
+                bkgbkg_eff = []
+                bkgbkg_pur = []
+                avg_sigsig_dist = []
+                avg_bkgbkg_dist = []
+                avg_sigbkg_dist = []
+                for k, k_value in enumerate(parameter_dict[parameter_k]["value"]):
+                    dim = assign_variable([args.fixed, parameter_j, parameter_k], [i_value, j_value, k_value], "dim")
+                    margin = assign_variable([args.fixed, parameter_j, parameter_k], [i_value, j_value, k_value], "margin")
+                    penalty = assign_variable([args.fixed, parameter_j, parameter_k], [i_value, j_value, k_value], "penalty")
+                    radius = margin/2
+                    json_file = model_path + "embedding_"+str(dim)+"feats/EmbeddingNet_m"+str(margin)+"_r"+str(radius)+"_Lambda"+str(penalty)+"_dict.json"
+                    with open(json_file, 'r') as file:
+                        graph_dict = json.load(file)
+                    same_class_eff.append(graph_dict["same_class_eff"])
+                    same_class_pur.append(graph_dict["same_class_purity"])
+                    avg_sigsig_dist.append(graph_dict["avg_sigsig_dist"])
+                    avg_bkgbkg_dist.append(graph_dict["avg_bkgbkg_dist"])
+                    avg_sigbkg_dist.append(graph_dict["avg_sigbkg_dist"])
+                eff_ax.plot(parameter_dict[parameter_k]["value"], same_class_eff, linestyle="-", marker='o', color=colours[j], label=label_j + ": " + str(j_value))
+                pur_ax.plot(parameter_dict[parameter_k]["value"], same_class_pur, linestyle="-", marker='o', color=colours[j], label=label_j + ": " + str(j_value))
+                for d, dist in enumerate([avg_sigsig_dist, avg_bkgbkg_dist, avg_sigbkg_dist]):
+                    dist_ax.plot(parameter_dict[parameter_k]["value"], dist, linestyle=linestyles[d], marker='o', color=colours[j])
+                linestyle_leg = [plt.Line2D([0], [0], color="black", linestyle=l) for l in linestyles]
+                colour_leg = [plt.Line2D([0], [0], lw=2, linestyle="-", marker='o', color=colours[v], label=label_j+": "+str(value)) for v, value in enumerate(parameter_dict[parameter_j]["value"])]
+                linestyle_legend = dist_ax.legend(handles=linestyle_leg, labels=s_type, fontsize=16, frameon=False, loc="upper center")
+                colour_legend = dist_ax.legend(handles=colour_leg, labels=[label_j + ": " + str(value) for value in parameter_dict[parameter_j]["value"]], fontsize=16, frameon=False, loc="upper left")
+                dist_ax.add_artist(linestyle_legend) 
+
+                eff_ax.legend(loc="best", fontsize=16)
+                eff_ax.set_xlabel(label_k, loc="right", fontsize=16)
+                eff_ax.set_ylabel("Same-class efficiency", loc="top", fontsize=16)
+                eff_ax.set_yscale("log")
+                os.makedirs(plot_path+"/eff/", exist_ok=True)
+                eff_fig.savefig(plot_path+"/eff/"+"eff_vs_"+parameter_j+"_"+parameter_k+"_"+args.fixed+str(i_value)+".pdf")
+
+                pur_ax.legend(loc="best", fontsize=16)
+                pur_ax.set_xlabel(label_k, loc="right", fontsize=16)
+                pur_ax.set_ylabel("Same-class purity", loc="top", fontsize=16)
+                pur_ax.set_yscale("log")
+                os.makedirs(plot_path+"/pur/", exist_ok=True)
+                pur_fig.savefig(plot_path+"/pur/"+"pur_vs_"+parameter_j+"_"+parameter_k+"_"+args.fixed+str(i_value)+".pdf")
+
+                dist_ax.set_xlabel(label_k, loc="right", fontsize=16)
+                dist_ax.set_ylabel("Average distance", loc="top", fontsize=16)
+                dist_ax.set_yscale("log")
+                os.makedirs(plot_path+"/dist/", exist_ok=True)
+                dist_fig.savefig(plot_path+"/dist/"+"dist_vs_"+parameter_j+"_"+parameter_k+"_"+args.fixed+str(i_value)+".pdf")
+            plt.close()
+
+if args.corr:
+    print("Plotting correlation map")
+    file_list = glob(model_path + "/*/*.json")
+    data = []
+    for file in file_list:
+        with open(file, 'r') as f:
+            data.append(json.load(f))
+    df = pd.DataFrame(data)
+    tick_labels = ["Margin", "Penalty", "Radius", "Dimension", 
+                   "Avg. sig-sig dist", "Avg. sig-bkg dist", "Avg. bkg-bkg dist",
+                   "Edge fraction", "Same class efficiency", "Same class purity", "Sig-sig efficiency", "Sig-sig purity", "Bkg-bkg efficiency", "Bkg-bkg purity"]
+    correlation_matrix = df.corr()
     
+    corr_fig, corr_ax = plt.subplots(figsize=(11, 9), constrained_layout=True)
+    sns.heatmap(correlation_matrix, annot=True, cbar=True, xticklabels=tick_labels, yticklabels=tick_labels, cmap="coolwarm")
+    os.makedirs(plot_path+"/corr/", exist_ok=True)
+    corr_plot_name = plot_path+"/corr/corr.pdf"
+    corr_fig.savefig(corr_plot_name)
 
-
-
-
-
-
-
-
-            
-
+    
