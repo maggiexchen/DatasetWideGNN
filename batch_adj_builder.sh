b327e0a5b7da58af37ac7ad0ece1d3aba8c76930
Holly Pacey <holly.ann.pacey@cern.ch>
2025-04-10 15:05:59 +0100

    adding basic batch-running scripts

---
+++ batch_adj_builder.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+# change to your own area to run from:
+cd /home/pacey/GNN/TestMphys/hhhgraph/
+
+source setup/setup_conda_env_Holly.sh
+
+# check chmod +x war run on your torch_train.py, and this script.
+python torch_adj_builder.py --MLconfig config/ml_default.yaml --user config/user_Holly.yaml 
+
