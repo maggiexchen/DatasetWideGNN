9e5612c0158b83da387c7c258d52a9302135fcfa
Maggie Chen <meiqi.chen@cern.ch>
2025-04-16 13:17:40 +0100

    correct standardisation

--- GraphBuilder/define_pair.py
+++ GraphBuilder/define_pair.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 import torch
-from torch.utils.data import Dataset, DataLoader
+from torch.utils.data import random_split, Dataset, DataLoader
 import os
 import sys
 parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
@@ -10,55 +10,90 @@ import utils.misc as misc
 import random
 
 class PairDataset(Dataset):
-    def __init__(self, signal_events, background_events, num_sig_samples, num_bkg_samples):
+    def __init__(self, signal_events, background_events, num_sig_samples, num_bkg_samples, standardise=True):
         self.signal_events = signal_events
         self.background_events = background_events
         self.num_sig_samples = num_sig_samples
         self.num_bkg_samples = num_bkg_samples
+        self.standardise = standardise
 
         # Assert that the number of features match
         assert signal_events.shape[1] == background_events.shape[1], "Signal and background events do not have the same number of features!"
         self.num_features = signal_events.shape[1]
 
         # Precompute pairs
-        self.pairs, self.means, self.stds = self.generate_pairs()
+        if self.standardise:
+            self.train_pairs, self.val_pairs, self.means, self.stds = self.generate_pairs()
+        else:
+            self.train_pairs, self, val_pairs = self.generate_pairs()
+            self.means = None
+            self.stds = None
 
     def generate_pairs(self):
         # Choose num_sig_samples and num_bkg_samples from the events
-        sig_idx = torch.randperm(len(self.signal_events))[:self.num_sig_samples]
-        bkg_idx = torch.randperm(len(self.background_events))[:self.num_bkg_samples]
+        sig_idx = torch.randperm(len(self.signal_events))
+        bkg_idx = torch.randperm(len(self.background_events))
 
-        sampled_sig_ind = torch.arange(self.num_sig_samples)
-        sampled_bkg_ind = torch.arange(self.num_bkg_samples)
+        sampled_train_sig_ind = sig_idx[:self.num_sig_samples]
+        sampled_train_bkg_ind = bkg_idx[:self.num_bkg_samples]
+        sampled_val_sig_ind = sig_idx[-int(self.num_sig_samples/2):]
+        sampled_val_bkg_ind = bkg_idx[-int(self.num_bkg_samples/2):]
 
         # standardising the sampled signal and background events
-        sampled_full_x = pd.DataFrame(torch.cat((self.signal_events[sampled_sig_ind], self.background_events[sampled_bkg_ind])).numpy())
-        means, stds = misc.get_train_mean_std(sampled_full_x)
-        # then standardise the full signal and background samples
-        self.signal_events = (self.signal_events - torch.Tensor(means))/torch.Tensor(stds)
-        self.background_events = (self.background_events - torch.Tensor(means))/torch.Tensor(stds)
+        if self.standardise:
+            print("Standardising ...")
+            sampled_full_x = pd.DataFrame(torch.cat((self.signal_events[sampled_train_sig_ind], self.background_events[sampled_train_bkg_ind])).numpy())
+            means, stds = misc.get_train_mean_std(sampled_full_x)
+            # then standardise the full signal and background samples
+            self.signal_events = (self.signal_events - torch.Tensor(means))/torch.Tensor(stds)
+            self.background_events = (self.background_events - torch.Tensor(means))/torch.Tensor(stds)
+        else:
+            means, stds = None, None
 
-        cartesian_product_sigsig_ind = torch.cartesian_prod(sampled_sig_ind, sampled_sig_ind)
-        cartesian_product_bkgbkg_ind = torch.cartesian_prod(sampled_bkg_ind, sampled_bkg_ind)
-        cartesian_product_sigbkg_ind = torch.cartesian_prod(sampled_sig_ind, sampled_bkg_ind)
-        pairs = []
+        print("Generating pairs ...")
+        train_cartesian_product_sigsig_ind = torch.cartesian_prod(sampled_train_sig_ind, sampled_train_sig_ind)
+        train_cartesian_product_bkgbkg_ind = torch.cartesian_prod(sampled_train_bkg_ind, sampled_train_bkg_ind)
+        train_cartesian_product_sigbkg_ind = torch.cartesian_prod(sampled_train_sig_ind, sampled_train_bkg_ind)
+        print("Train pairs", len(train_cartesian_product_sigsig_ind), len(train_cartesian_product_bkgbkg_ind), len(train_cartesian_product_sigbkg_ind))
 
-        for idx1, idx2 in cartesian_product_sigsig_ind:
-            pairs.append((self.signal_events[sig_idx[idx1]], self.signal_events[sig_idx[idx2]], 1))
+        val_cartesian_product_sigsig_ind = torch.cartesian_prod(sampled_val_sig_ind, sampled_val_sig_ind)
+        val_cartesian_product_bkgbkg_ind = torch.cartesian_prod(sampled_val_bkg_ind, sampled_val_bkg_ind)
+        val_cartesian_product_sigbkg_ind = torch.cartesian_prod(sampled_val_sig_ind, sampled_val_bkg_ind)
+        print("Val pairs", len(val_cartesian_product_sigsig_ind), len(val_cartesian_product_bkgbkg_ind), len(val_cartesian_product_sigbkg_ind))
 
-        for idx1, idx2 in cartesian_product_bkgbkg_ind:
-            pairs.append((self.background_events[bkg_idx[idx1]], self.background_events[bkg_idx[idx2]], 1))
+        train_pairs = []
+        val_pairs = []
 
-        for idx1, idx2 in cartesian_product_sigbkg_ind:
-            pairs.append((self.signal_events[sig_idx[idx1]], self.background_events[bkg_idx[idx2]], 0))
+        for idx1, idx2 in train_cartesian_product_sigsig_ind:
+            train_pairs.append((self.signal_events[sig_idx[idx1]], self.signal_events[sig_idx[idx2]], 1))
 
-        return pairs, torch.Tensor(means), torch.Tensor(stds)
+        for idx1, idx2 in train_cartesian_product_bkgbkg_ind:
+            train_pairs.append((self.background_events[bkg_idx[idx1]], self.background_events[bkg_idx[idx2]], 1))
+
+        for idx1, idx2 in train_cartesian_product_sigbkg_ind:
+            train_pairs.append((self.signal_events[sig_idx[idx1]], self.background_events[bkg_idx[idx2]], 0))
+        
+        for idx1, idx2 in val_cartesian_product_sigsig_ind:
+            val_pairs.append((self.signal_events[sig_idx[idx1]], self.signal_events[sig_idx[idx2]], 1))
+
+        for idx1, idx2 in val_cartesian_product_bkgbkg_ind:
+            val_pairs.append((self.background_events[bkg_idx[idx1]], self.background_events[bkg_idx[idx2]], 1))
+
+        for idx1, idx2 in val_cartesian_product_sigbkg_ind:
+            val_pairs.append((self.signal_events[sig_idx[idx1]], self.background_events[bkg_idx[idx2]], 0))
+
+        print("Finished appending pairs ...")
+        if self.standardise:
+            return train_pairs, val_pairs, torch.Tensor(means), torch.Tensor(stds)
+        else:
+            return train_pairs, val_pairs
 
     def __len__(self):
-        return len(self.pairs)
+        return len(self.train_pairs), len(self.val_pairs)
 
     def __getitem__(self, idx):
-        signal_event, background_event, label = self.pairs[idx]
+        train_signal_event, train_background_event, train_label = self.train_pairs[idx]
+        val_signal_event, val_background_event, val_label = self.val_pairs[idx]
         means = self.means
         stds = self.stds
-        return signal_event, background_event, label, means, stds
+        return train_signal_event, train_background_event, val_signal_event, val_background_event, train_label, val_label, means, stds
