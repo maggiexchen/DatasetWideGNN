552c769a9e8b82cfd52aa5c3b137e185f375343e
Ruben Honscheid <ruben.honscheid@physics.ox.ac.uk>
2025-11-26 15:35:46 +0000

    Reset configs to master default

--- batch_scripts/batch_train.sh
+++ batch_scripts/batch_train.sh
@@ -3,9 +3,9 @@ echo "ml config " $1
 echo "userconfig " $2
 
 # change to your own area to run from:
-cd /data/atlas/users/honscheid/HHH_GNN/hhhgraph
+cd /home/pacey/GNN/new_merge/hhhgraph/
 
-source /data/atlas/users/honscheid/HHH_GNN/hhhgraph/setup/setup_conda_env_Ruben.sh
+source setup/setup_conda_env_Holly.sh
 
 # check chmod +x war run on your torch_train.py, and this script.
 python torch_train.py --MLconfig $1 --user $2
