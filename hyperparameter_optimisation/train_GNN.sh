da30f0cf6be60e87ca754e7dbea53abdfc7a2aac
Maggie Chen <meiqi.chen@cern.ch>
2026-01-21 15:20:26 +0000

    hyperparameter scan bash & config scripts

---
+++ hyperparameter_optimisation/train_GNN.sh
@@ -0,0 +1,120 @@
+#!/bin/bash
+
+BASE_DIR="/data/atlas/atlasdata3/maggiechen/gnn_project"
+# Define the path to the user config & ml training file
+
+USER_CONFIG_FILE=$BASE_DIR"/hyperparameter_optimisation/config/user_Maggie_GNN_scan.yaml"
+ML_CONFIG_FILE=$BASE_DIR"/hyperparameter_optimisation/config/ml_LQ_GNN_scan.yaml"
+
+# GNN parameters to fix
+VAR_LEVEL=$1 # LQ_LowLevel or LQ_HighLevel
+MODEL=$2 # DNN, GCN or Graph
+DISTANCE=$3 # euclidean, cosine, or emd
+EDGE_FRAC=0.1
+EPOCH=5
+SINGLEFOLD=1
+VALFRAC=4
+
+sed -i "s|^kinematic_variable: .*|kinematic_variable: $VAR_LEVEL|" "$ML_CONFIG_FILE"
+sed -i "s|^gnn_type: .*|gnn_type: $MODEL|" "$ML_CONFIG_FILE"
+sed -i "s|^distance: .*|distance: $DISTANCE|" "$ML_CONFIG_FILE"
+sed -i "s|^edge_frac: .*|edge_frac: $EDGE_FRAC|" "$ML_CONFIG_FILE"
+sed -i "s|^epochs: .*|epochs: $EPOCH|" "$ML_CONFIG_FILE"
+sed -i "s|^single_fold: .*|single_fold: $SINGLEFOLD|" "$ML_CONFIG_FILE"
+sed -i "s|^n_fold: .*|n_fold: $VALFRAC|" "$USER_CONFIG_FILE"
+
+# Parameters to optimise
+# BATCHSIZE=(1024 2048)
+# DROPOUT=(0)
+# LR=(0.0005 0.001 0.005 0.1)
+# LR_PATIENCE=(3)
+
+# MLP_HIDDEN_NODES=(5 10 15 20)
+# MLP_LAYERS=(2 3 4)
+
+# GNN_HIDDEN_NODES=(5 10 15 20)
+# GNN_LAYERS=(2 3 4)
+# NEIGHBOURS=(4 8 16 32)
+# NEIGHBOURS_LAYERS=(2 4 6)
+
+BATCHSIZE=(1024)
+DROPOUT=(0)
+LR=(0.001)
+LR_PATIENCE=(3)
+
+MLP_HIDDEN_NODES=(5 10)
+MLP_LAYERS=(3)
+GNN_HIDDEN_NODES=(5 10 15 20)
+GNN_LAYERS=(2 3 4)
+NEIGHBOURS=(4 8 16 32)
+NEIGHBOURS_LAYERS=(2 4 6)
+
+# Define model dir and file name
+SAVE_DIR="/data/atlas/atlasdata3/maggiechen/gnn_project/hyperparameter_optimisation"
+model_save_path=$SAVE_DIR"/"$MODEL"_"$VAR_LEVEL"_"$DISTANCE"_EdgeFrac"$EDGE_FRAC"/models/"
+plot_save_path=$SAVE_DIR"/"$MODEL"_"$VAR_LEVEL"_EdgeFrac"$EDGE_FRAC"/plots/"
+sed -i "s|^model_path: .*|model_path: $model_save_path|" "$USER_CONFIG_FILE"
+sed -i "s|^plot_path: .*|plot_path: $plot_save_path|" "$USER_CONFIG_FILE"
+
+# Loop through the parameter values
+# First loop through the LR, LR patience, dropout, batchsize
+for lr_pat in "${LR_PATIENCE[@]}"; do
+    for dropout in "${DROPOUT[@]}"; do
+        for bs in "${BATCHSIZE[@]}"; do
+            for lr in "${LR[@]}"; do
+                sed -i "s|^patience_LR: .*|patience_LR: $lr_pat|" "$ML_CONFIG_FILE"
+                echo "LA PAT"
+                sed -i "s|^LR: .*|LR: $lr|" "$ML_CONFIG_FILE"
+                echo "LR"
+                sed -i "s|^batch_size: .*|batch_size: $bs|" "$ML_CONFIG_FILE"
+                echo "BATCH SIZE"
+                for dnn_nodes in "${MLP_HIDDEN_NODES[@]}"; do
+                    for dnn_layers in "${MLP_LAYERS[@]}"; do
+                        mlp_hidden_layers="["
+                        dropout_rates="["
+                        for ((i=1; i<=dnn_layers; i++)); do
+                            mlp_hidden_layers+="$dnn_nodes"
+                            if [ $i -lt $dnn_layers ]; then
+                                mlp_hidden_layers+=", "
+                            fi
+                        done
+                        for ((i=1; i<=dnn_layers; i++)); do
+                            dropout_rates+="$dropout"
+                            if [ $i -lt $dnn_layers ]; then
+                            dropout_rates+=", "
+                            fi
+                        done
+                        mlp_hidden_layers+="]"
+                        sed -i "s|^hidden_sizes_mlp: .*|hidden_sizes_mlp: $mlp_hidden_layers|" "$ML_CONFIG_FILE"
+                        echo "Training with MLP layers: " $mlp_hidden_layers 
+
+                        for gnn_nodes in "${GNN_HIDDEN_NODES[@]}"; do
+                            for gnn_layers in "${GNN_LAYERS[@]}"; do
+                            gnn_hidden_layers="["
+                                for ((i=1; i<=gnn_layers; i++)); do
+                                    gnn_hidden_layers+="$gnn_nodes"
+                                    if [ $i -lt $gnn_layers ]; then
+                                    gnn_hidden_layers+=", "
+                                    fi
+                                done
+                                for ((i=1; i<=dnn_layers+gnn_layers; i++)); do
+                                    dropout_rates+="$dropout"
+                                    if [ $i -lt $gnn_layers+$dnn_layers ]; then
+                                    dropout_rates+=", "
+                                    fi
+                                done
+                                dropout_rates+="]"
+                                gnn_hidden_layers+="]"
+                                sed -i "s|^hidden_sizes_gcn: .*|hidden_sizes_gcn: $gnn_hidden_layers|" "$ML_CONFIG_FILE"
+                                echo "Training with GNN layers: " $gnn_hidden_layers          
+                                sed -i "s|^dropout_rates: .*|dropout_rates: $dropout_rates|" "$ML_CONFIG_FILE"
+                                
+                                python $BASE_DIR"/torch_train.py" -u"$USER_CONFIG_FILE" -c "$ML_CONFIG_FILE"
+                            done
+                        done
+                    done
+                done
+            done
+        done
+    done
+done
